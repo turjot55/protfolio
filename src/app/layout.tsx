fcd718d8b2543f7b65959c9a7b1a73d23f3fb3c7
Turja Talukder <turjatalukder@Turjas-MacBook-Air.local>
2025-04-07 17:45:08 +0600

    metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Inter, Calistoga} from 'next/font/google'
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans" });
 const calistoga = Calistoga({ subsets: ['latin'], variable: "--font-sarif", weight: ["400"] });
@@ -10,7 +11,7 @@ const calistoga = Calistoga({ subsets: ['latin'], variable: "--font-sarif", weig
 
 export const metadata: Metadata = {
   title: "Turja Talukder",
-  description: "Created with the help of Frontend Tribe",
+  description: "Created with Love by Turja Talukder",
 };
 
 export default function RootLayout({
@@ -20,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.variable, calistoga.variable,"bg-gray-900 text-white antialiased font-sans")}>{children}</body>
-    </html>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>
+      {children}
+    </body>
+  </html>
   );
 }
